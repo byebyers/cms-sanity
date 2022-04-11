32409cce7e72db99ebc97f5bf38b8da81a93fb24
Jacob Byers <byers.jacob@gmail.com>
2022-04-11 15:42:19 -0700

    resolve url

--- sanity/resolveProductionUrl.js
+++ sanity/resolveProductionUrl.js
@@ -1,6 +1,6 @@
 const previewSecret = 'asf67asdf768a' // Copy the string you used for SANITY_PREVIEW_SECRET
 const localUrl = 'http://localhost:3000'
-const remoteUrl = `https://cms-sanity-muk73zkme-byebyers.vercel.app`
+const remoteUrl = `https://cms-sanity-six.vercel.app/`
 
 export default function resolveProductionUrl(document) {
   const baseUrl = window.location.hostname === 'localhost' ? localUrl : remoteUrl
